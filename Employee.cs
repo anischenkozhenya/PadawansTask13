b40cf223e47f62ae749c5426d1b0c5f9808e850b
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-17 11:48:52 +0300

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string Surname;
+        private int Age;
+        public Employee()
+        {
+        }
+        public Employee(int age, string surname)
+        {
+            Age = age;
+            Surname = surname;
+        }
+        private string GetAge()
+        {
+            string res = Age.ToString();
+            return res;
+        }
+        public void SetNewSurname(string newsurname)
+        {
+            Surname = newsurname;
+        }
+        public string GetInfo()
+        {
+            string Info = "Surname: " + this.Surname + ",Age: " + this.Age;
+            return Info;
+        }
+    }
+}
